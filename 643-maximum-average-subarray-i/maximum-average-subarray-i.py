b4f5a4d501c3faa1a6e7b01acdf913e3caa2b2aa
Pilla Dinesh Kumar <pilladinesh582@gmail.com>
2026-06-11 21:59:22 +0530

    Time: 58 ms (67.67%) | Memory: 29 MB (75.44%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -3,9 +3,7 @@ class Solution:
         n=len(nums)
         win=sum(nums[:k])
         max_sum=win
- 
         for i in range(k,n):
             win+=nums[i]-nums[i-k]
             max_sum=max(max_sum,win)
-            
         return max_sum/k
