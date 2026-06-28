ef0e7e06b7478e20086b255876f9c29f32af4608
Pilla Dinesh Kumar <pilladinesh582@gmail.com>
2026-06-28 20:44:26 +0530

    Time: 52 ms (47.78%) | Memory: 28.5 MB (74.88%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        m1=float("inf")
-        m2=0
+        o1=float("inf")
+        o2=0
         for i in prices:
-             if i<m1:
-                m1=i
-             else:
-                p=i-m1
-                m2=max(p,m2)
-        return m2
+            o1=min(o1,i)
+            o2=max(o2,i-o1)
+        return o2
