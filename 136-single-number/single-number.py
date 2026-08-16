fd39887c91aa3ed097ebdc683dc9ab33dc323030
Pilla Dinesh Kumar <pilladinesh582@gmail.com>
2026-08-16 17:59:42 +0530

    Time: 7 ms (29.50%) | Memory: 21.7 MB (10.28%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,8 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         dic={}
-        for ch in nums:
-            dic[ch]=dic.get(ch,0)+1
-        for i,ch in enumerate(nums):
-            if dic[ch]==1:
-                return nums[i]
+        for i in nums:
+            dic[i]=dic.get(i,0)+1
+        for key,value in enumerate(nums):
+            if dic[value]==1:
+                return value
