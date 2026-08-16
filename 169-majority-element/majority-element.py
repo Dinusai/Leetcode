3232226b6732ed1db1386699714ffe752b439a77
Pilla Dinesh Kumar <pilladinesh582@gmail.com>
2026-08-16 22:30:12 +0530

    Time: 9 ms (37.02%) | Memory: 21.3 MB (18.49%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,6 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        n=len(nums)
         dic={}
         for i in nums:
             dic[i]=dic.get(i,0)+1
-        return max(dic,key=dic.get)
+        for key,value in enumerate(nums):
+            if dic[value]>n//2:
+                return value
